ccee0fa0910ac60ef23f9552eb24a20269583719
Artur Rodrigues <arturhoo@gmail.com>
2012-06-07 13:35:27 -0300

    changing urls to reflect new nginx configuration

--- read_api/read_api/urls.py
+++ read_api/read_api/urls.py
@@ -19,5 +19,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-    (r'^api/', include(v1_api.urls)),
+    (r'^read_api/api/', include(v1_api.urls)),
 )
